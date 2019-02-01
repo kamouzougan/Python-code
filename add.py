fc7b9c6fa8b5dc98a51f5ec4b58b5aab458b1d44
kamou <kamouzougan@gmail.com>
2019-02-01 13:01:41 -0500

    first project

---
+++ add.py
@@ -0,0 +1,6 @@
+#This program is to compute the addition
+num1 = int(input("Please enter your first number:"))
+num2 = int(input("Please enter your second  number:"))
+sum = num1 + num2
+print("The total of the sum of num1 and num2 is : sum")
+
